fcdfec164e7b123bfe78ac61b40f629ec3d2bb0f
Alexandre Marcelino <amsantos9@gmail.com>
2021-11-25 03:59:40 -0300

    Function to reuse instantiation

--- src/model/dao/impl/PacienteDaoJDBC.java
+++ src/model/dao/impl/PacienteDaoJDBC.java
@@ -40,7 +40,8 @@ public class PacienteDaoJDBC implements PacienteDao {
 		// TODO Auto-generated method stub
 		
 	}
-
+	
+	//MÉTODO PARA BUSCAR NO BANCO DE DADOS UM PACIENTE PELO O ID 
 	@Override
 	public Paciente findById(Integer id) {
 		PreparedStatement st = null;
@@ -56,22 +57,8 @@ public class PacienteDaoJDBC implements PacienteDao {
 			st.setInt(1, id);
 			rs = st.executeQuery();
 			if(rs.next()) {
-				Vacina vac = new Vacina();
-				vac.setId(rs.getInt("Id_Vacina"));
-				vac.setMarca(rs.getString("Marca"));
-				vac.setNome(rs.getString("Nome_da_Vacina"));
-				Paciente pac = new Paciente();
-				pac.setId(rs.getInt("Id"));
-				pac.setCpf(rs.getString("CPF"));
-				pac.setNome(rs.getString("Nome"));
-				pac.setIdade(rs.getInt("Idade"));
-				pac.setFone(rs.getString("Telefone"));
-				pac.setEndereco(rs.getString("Endereço"));
-				pac.setRegiao(rs.getString("Regiao"));
-				pac.setVacinado(rs.getString("Vacinado"));
-				pac.setData(rs.getDate("Data_da_Vacinacao"));
-				pac.setDose(rs.getInt("Dose"));
-				pac.setVacina(vac);
+				Vacina vac = instantiateVacina(rs);
+				Paciente pac = instantiatePaciente(rs, vac);
 				return pac;
 			}
 			return null;			
@@ -90,52 +77,35 @@ public class PacienteDaoJDBC implements PacienteDao {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	
+	//MÉTODO PARA BUSCAR NO BANCO DE DADOS PACIENTES QUE FORAM VACINADAS POR UMA DETERMINADA VACINA
 	@Override
 	public List<Paciente> findByVacina(Vacina vacina) {
 		PreparedStatement st = null;
-		ResultSet rs = null;
-		
+		ResultSet rs = null;		
 		try {
 			st = conn.prepareStatement
 					("SELECT tbpaciente.*, tbvacina.Marca, tbvacina.Nome_da_Vacina "
-					 + "FROM tbpaciente INNER JOIN tbvacina "
-					 + "ON tbpaciente.Id_Vacina = tbvacina.Id "
-					 + "WHERE Id_Vacina = ? "
-					 + "ORDER BY Nome");
-			
+							+ "FROM tbpaciente INNER JOIN tbvacina "
+							+ "ON tbpaciente.Id_Vacina = tbvacina.Id "
+							+ "WHERE Id_Vacina = ? "
+							+ "ORDER BY Nome");
+
 			st.setInt(1, vacina.getId());
-			
 			rs = st.executeQuery();
-			
+
 			List<Paciente> list = new ArrayList<>();
 			Map<Integer, Vacina> map = new HashMap<>();
-			
+
 			while(rs.next()) {
-				
 				Vacina vac = map.get(rs.getInt("Id_Vacina"));
-				
+
 				if(vac == null) {					
-				vac = new Vacina();
-				vac.setId(rs.getInt("Id_Vacina"));
-				vac.setMarca(rs.getString("Marca"));
-				vac.setNome(rs.getString("Nome_da_Vacina"));
-				map.put(rs.getInt("Id_Vacina"), vac);
+					vac = instantiateVacina(rs);
+					map.put(rs.getInt("Id_Vacina"), vac);
 				}
-				Paciente pac = new Paciente();
-				pac.setId(rs.getInt("Id"));
-				pac.setCpf(rs.getString("CPF"));
-				pac.setNome(rs.getString("Nome"));
-				pac.setIdade(rs.getInt("Idade"));
-				pac.setFone(rs.getString("Telefone"));
-				pac.setEndereco(rs.getString("Endereço"));
-				pac.setRegiao(rs.getString("Regiao"));
-				pac.setVacinado(rs.getString("Vacinado"));
-				pac.setData(rs.getDate("Data_da_Vacinacao"));
-				pac.setDose(rs.getInt("Dose"));
-				pac.setVacina(vac);
+				Paciente pac = instantiatePaciente(rs, vac);
 				list.add(pac);
-				
 			}
 			return list;			
 		}
@@ -146,8 +116,31 @@ public class PacienteDaoJDBC implements PacienteDao {
 			DB.closeStatement(st);
 			DB.closeResultSet(rs);
 		}
-			
-			
-			
 	}
+	
+	//FUNÇÃO AUXILIAR PARA INSTANCIAR A CLASSE PACIENTE
+	private Paciente instantiatePaciente(ResultSet rs, Vacina vac) throws SQLException {
+		Paciente pac = new Paciente();
+		pac.setId(rs.getInt("Id"));
+		pac.setCpf(rs.getString("CPF"));
+		pac.setNome(rs.getString("Nome"));
+		pac.setIdade(rs.getInt("Idade"));
+		pac.setFone(rs.getString("Telefone"));
+		pac.setEndereco(rs.getString("Endereço"));
+		pac.setRegiao(rs.getString("Regiao"));
+		pac.setVacinado(rs.getString("Vacinado"));
+		pac.setData(rs.getDate("Data_da_Vacinacao"));
+		pac.setDose(rs.getInt("Dose"));
+		pac.setVacina(vac);
+		return pac;
+	}
+	
+	//FUNÇÃO AUXILIAR PARA INSTANCIAR A CLASSE VACINA
+	private Vacina instantiateVacina(ResultSet rs) throws SQLException {
+		Vacina vac = new Vacina();
+		vac.setId(rs.getInt("Id_Vacina"));
+		vac.setMarca(rs.getString("Marca"));
+		vac.setNome(rs.getString("Nome_da_Vacina"));
+		return vac;
+	}	
 }
